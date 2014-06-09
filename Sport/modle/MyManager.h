4aa7df9aef4c2b2c96d671a1197f84a7ce7f84c5
zltqzj <zltqzj@163.com>
2014-06-09 23:49:06 +0800

    点击开始才划线，切换到非MAP界面，不划线。

---
+++ Sport/modle/MyManager.h
@@ -0,0 +1,13 @@
+//
+//  MyManager.h
+//  Sport
+//
+//  Created by ZKR on 6/9/14.
+//  Copyright (c) 2014 ZKR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyManager : NSObject
+
+@end
---
+++ Sport/modle/MyManager.m
@@ -0,0 +1,13 @@
+//
+//  MyManager.m
+//  Sport
+//
+//  Created by ZKR on 6/9/14.
+//  Copyright (c) 2014 ZKR. All rights reserved.
+//
+
+#import "MyManager.h"
+
+@implementation MyManager
+
+@end
